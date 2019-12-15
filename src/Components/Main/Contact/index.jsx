52a6052d57760860a93648358a0d5920064969ea
jenyapetrosian <jenniepetrosian@gmail.com>
2019-12-16 02:57:49 +0400

    Contact Us section

--- src/Components/Main/Contact/index.jsx
+++ src/Components/Main/Contact/index.jsx
@@ -1,10 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react'
+import {selectLanguage} from '../../../translate'
 import './style.scss'
+import { contactIcon, contactBgImage } from '../../../../src/images/index'
+import BasicTextFields from './input/index'
 
-export default class About extends Component{
-    render(){
-        return(
-            <div>Contact</div>
-        )
-    }
+const Contact = ({lang}) => {
+
+    return <div className='contain contactUs'>
+        <img src={contactBgImage} alt='background' className='contactBgImg' />
+        <div className='contactUsWrapper flexible horizontal jBetween'>
+            <div className='messageBoxContainer flexible vertical jStart'>
+                <div className='textContainer flexible vertical aStart'>   
+                    <p className='preHeaderText'>{selectLanguage(lang).contact_us}</p>
+                    <h2 className='headerText'>{selectLanguage(lang).get_in_touch}</h2>  
+                    <p className='randomText'>{selectLanguage(lang).please_feel_free_to_contact_us_through_our_support_center}</p>
+                </div>
+                <div className='messageBox'>
+                    <p>{selectLanguage(lang).send_us_a_message}</p>
+                    <BasicTextFields />
+                </div>
+            </div>
+            <div className='contactUsImgContainer'>
+                <img src={contactIcon} alt='contact us' className='contactIcon' />
+            </div>
+        </div>
+    </div>
 }
+
+export default Contact
--- src/translate.js
+++ src/translate.js
@@ -8,7 +8,11 @@ const languages =  {
         "Contact":"Կապ մեզ հետ",
         "JoinUs":"Միացեք մեզ",
         "our_amazing_features": "Մեր բացառիկ առանձնահատկությունները",
-        "travel_for_everyone": "Ճանապարհորդություն բոլորի համար"
+        "travel_for_everyone": "Ճանապարհորդություն բոլորի համար",
+        "contact_us": "Կապ մեզ հետ",
+        "get_in_touch": "Եղիր կապի մեջ",
+        "please_feel_free_to_contact_us_through_our_support_center": "Դուք կարող եք կապ հաստատել մեր կոնտակտ կենտրոնի հետ",
+        "send_us_a_message": "Գրեք մեզ"
     },
     ru: {
         "hi": "Здравствуй",
@@ -17,9 +21,13 @@ const languages =  {
         "Plans": "Планы",
         "Blog":"Блог",
         "Contact":"Контакты",
-        "JoinUs":"Присоединяться",
+        "JoinUs":"Присоединиться",
         "our_amazing_features": "Наши удивительные особенности",
-        "travel_for_everyone": "Путешествия для всех"
+        "travel_for_everyone": "Путешествия для всех",
+        "contact_us": "Связаться с нами",
+        "get_in_touch": "Будьте на связи",
+        "please_feel_free_to_contact_us_through_our_support_center": "Вы можете свободно обращаться к нам через наш центр поддержки",
+        "send_us_a_message": "Напишите нам письмо"
     },
     en: {
         "hi": "Hi",
@@ -30,7 +38,11 @@ const languages =  {
         "Contact":"Contact",
         "JoinUs":"Join us",
         "our_amazing_features": "OUR AMAZING FEATURES",
-        "travel_for_everyone": "travel for everyone"
+        "travel_for_everyone": "travel for everyone",
+        "contact_us": "Contact us",
+        "get_in_touch": "Get in touch",
+        "please_feel_free_to_contact_us_through_our_support_center": "Please feel free to contact us through our support center",
+        "send_us_a_message": "Send Us a Message"
     }
 }
 
