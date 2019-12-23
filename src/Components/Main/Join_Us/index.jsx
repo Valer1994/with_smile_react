fad86ac1cc30d729b355c6ce68fe3405be126e8a
Micko1994 <mayklgevkur.94@mail.ru>
2019-12-23 19:58:18 +0400

    implament redux react

--- src/Components/Main/Join_Us/index.jsx
+++ src/Components/Main/Join_Us/index.jsx
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+import {
+    changeName,
+    changeSurName
+} from '../../../Actions/'
+
 import './style.scss'
 
-export default class About extends Component{
+const mapStateToProps = ({ counter }) => ({ 
+    name: counter.name, 
+    surname: counter.surname 
+})
+
+class JoinUs extends Component{
+    state = {
+        name: 'Poxos',
+        surname: 'Poxosyan'
+    }
+
+    nameHandler(name){
+        console.log('namename:', name)
+        this.props.changeName(name)
+        console.log('this.props.:', this.props)
+    }
+
     render(){
         return(
-            <div>Join Us</div>
+            <div>
+                <span>my name is: {this.props.name}</span>
+                <br/>
+                <span>my surname is: {this.props.surname}</span>
+                <br/>
+                <button 
+                    onClick={() => this.nameHandler(4)}>
+                        Change name and surname
+                </button>
+            </div>
         )
     }
 }
+
+
+export default connect(mapStateToProps, 
+    { changeName, changeSurName})(JoinUs)
